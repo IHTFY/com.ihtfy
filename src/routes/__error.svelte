9d0be422bc61880768fe22e3b727a7b179f7f77c
Frankie Mercado <oeluer@gmail.com>
2022-04-23 16:53:26 -0700

    type to lang as per svelte preprocess update

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,7 +1,7 @@
 <svelte:head>
 	<meta charset="UTF-8" />
 	<meta http-equiv="refresh" content="0; url=/" />
-	<script type="text/javascript">
+	<script lang="javascript">
 		window.location.href = '/';
 	</script>
 	<title>IHTFY | Page Not Found</title>
